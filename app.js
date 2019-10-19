89c1600f938eb638fcc5b40c16e4211dcee8b5cb
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-10-19 13:42:13 -0500

    going through lab objectives

---
+++ app.js
@@ -0,0 +1,31 @@
+// setName();
+// function setName() {
+//   var name = 'Seth';
+//   console.log(name);
+// }
+
+// console.log('some text');
+// let avg = findAvg(2, 2);
+// console.log('some text', avg);
+// function findAvg(a, b) {
+//     console.log('some text');
+//     var answer = ( a + b) / 2;
+//     return answer;
+// }
+
+let fruits = ['apple', 'orange', 'grape'];
+
+function printFruit() {
+  let favFruit = fruits[1];
+  console.log(fruits[0]);
+
+  function printFavFruit() {
+    console.log(favFruit);
+  }
+  printFavFruit();
+}
+
+
+
+printFruit();
+printFavFruit();
