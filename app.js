8b916a77c22faf3859fc61864d908a5e576fca0a
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-10-21 20:05:47 -0500

    finished last few objectives on scope

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-// setName();
-// function setName() {
-//   var name = 'Seth';
-//   console.log(name);
-// }
+setName();
+function setName() {
+  var name = 'Seth';
+  console.log(name);
+}
 
-// console.log('some text');
-// let avg = findAvg(2, 2);
-// console.log('some text', avg);
-// function findAvg(a, b) {
-//     console.log('some text');
-//     var answer = ( a + b) / 2;
-//     return answer;
-// }
+console.log('some text');
+let avg = findAvg(2, 2);
+console.log('some text', avg);
+function findAvg(a, b) {
+    console.log('some text');
+    var answer = ( a + b) / 2;
+    return answer;
+}
 
 let fruits = ['apple', 'orange', 'grape'];
 
@@ -19,15 +19,27 @@ let fruits = ['apple', 'orange', 'grape'];
 
 function printFruit() {
   let favFruit = fruits[1];
+
   console.log(fruits[0]);
   function printFavFruit() {
+    let leastFav = fruits[0];
     console.log(favFruit);
   }
-  printFavFruit();
   console.log(leastFav);
+  printFavFruit();
 }
 
-let leastFav = fruits[0];
 
 
 printFruit();
+
+randoFunc();
+
+function randoFunc() {
+  console.log('Hello Seth!');
+}
+
+let anotherFunc = function() {
+  alert('I hope this works...');
+}
+anotherFunc();
